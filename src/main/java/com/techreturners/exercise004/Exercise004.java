2d35c14e936cd1defd83e0333c089ec6adf33bd0
vllavanya <65501898+vllavanya@users.noreply.github.com>
2020-11-24 01:09:33 +0000

    Lavanya exercise004
    Logic Added

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -7,6 +7,20 @@ public class Exercise004 {
 
     public String pigLatin(String str) {
         // Add your code here
-        return "";
+          return "";
+        StringBuffer s2 = new StringBuffer();
+        char[] input = str.toCharArray();
+        char c = ' ';
+        for (int i = 0; i < input.length; i++) {
+            if (i == 0) {
+                c = input[i];
+            } else {
+                s2.append(input[i]);
+            }
+        }
+        s2.append(c);
+        s2.append("ay");
+        return "" + s2;
+         
     }
 }
